0fb3e5ebb88b6585a08906f082b3f9ef4f9cbf47
izumi_yoshiki <izumiyoshiki@icloud.com>
2019-05-14 20:38:11 +0900

    Initial Commit

---
+++ OpenCVTest/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  OpenCVTest
+//
+//  Created by Yoshiki Izumi on 2019/05/14.
+//  Copyright © 2019 Yoshiki Izumi. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
